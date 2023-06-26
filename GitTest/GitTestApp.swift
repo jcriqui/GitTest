a74d66aae30fbcfd0d1418389dfc954945437809
jcriqui <jasminecriqui@gmail.com>
2023-06-26 10:52:31 -0700

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Jasmine Criqui on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
